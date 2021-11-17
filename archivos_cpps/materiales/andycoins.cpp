faa710ca8747976700ed091d5d5d5afd353b8159
Franco Losardo <francolosa@hotmail.com>
2021-11-17 16:01:11 -0300

    implemento metodo en los dos materiales q faltaban

--- archivos_cpps/materiales/andycoins.cpp
+++ archivos_cpps/materiales/andycoins.cpp
@@ -1,7 +1,11 @@
 #include "../../archivos_h/materiales/andycoins.h"
 
+using namespace std;
+
 Andycoins::Andycoins(unsigned int cantidad_inventario){
     nombre = ANDYCOINS;
     cantidad = cantidad_inventario;
 }
-void Andycoins::mostrar_material(){}
+void Andycoins::mostrar_material(){
+    cout << "--> Material: andycoins, cantidad: " << this -> cantidad << endl;
+}
--- archivos_cpps/materiales/bomba.cpp
+++ archivos_cpps/materiales/bomba.cpp
@@ -1,7 +1,11 @@
 #include "../../archivos_h/materiales/bomba.h"
 
+using namespace std;
+
 Bomba::Bomba(unsigned int cantidad_inventario){
     nombre = BOMBA;
     cantidad = cantidad_inventario;
 }
-void Bomba::mostrar_material() {}
+void Bomba::mostrar_material() {
+    cout << "--> Material: bomba, cantidad: " << this -> cantidad << endl;
+}
