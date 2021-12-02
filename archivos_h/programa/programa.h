88679646d5f6e50373a8b32771d7ad772ad63eae
Brayan Saiago <bsaiago@fi.uba.ar>
2021-12-02 22:24:37 +0100

    Mejoras clase programa y menu

---
+++ archivos_h/programa/programa.h
@@ -0,0 +1,43 @@
+#ifndef _PROGRAMA_H_
+#define _PROGRAMA_H_
+
+#include "../menu/menu_configuracion.h"
+#include "../menu/menu_partida.h"
+#include "../carga_de_datos/cargar_edificios.h"
+#include "../carga_de_datos/cargar_mapa.h"
+#include "../carga_de_datos/cargar_materiales.h"
+#include "../carga_de_datos/cargar_ubicaciones.h"
+
+class Programa {
+
+    private:
+
+        Menu_configuracion menu_configuracion;
+        Menu_partida menu_partida;
+        Cargar_materiales materiales;
+        Cargar_edificios edificios;
+        Cargar_mapa mapa;
+        Cargar_ubicaciones ubicacion;
+        Andypolis juego;
+        bool existe_ubicaciones;
+    
+    public:
+
+        /*
+        * Post: Creará un objeto de tipo programa, dejando los atributos correctamente cargados con sus valores por defecto.
+        */
+        Programa();
+
+        /*
+        * Post: Emperzará el programa.
+        */
+        void empezar();
+
+        /*
+        * Post: Finalizará el programa, dejandolo con todos los cambios realizados actualizados.
+        */
+        void finalizar();
+
+};
+
+#endif //_PROGRAMA_H_
